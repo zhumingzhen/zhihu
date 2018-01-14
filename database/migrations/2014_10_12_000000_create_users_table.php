4b9a1aac34ea032152b3ee55f57f8b1b39223f48
zhumingzhen <itdream53@gmail.com>
2018-01-14 23:58:03 +0800

    迁移文件

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -28,7 +28,7 @@ class CreateUsersTable extends Migration
             $table->integer('likes_count')->default(0);
             $table->integer('followers_count')->default(0);
             $table->integer('followings_count')->default(0);
-            $table->json('settings')->nullabel();
+            $table->string('settings')->nullabel();
             $table->rememberToken();
             $table->timestamps();
         });
