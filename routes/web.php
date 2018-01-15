0f50669ccf56a02588e962741686b589cb0c133d
zhumingzhen <itdream53@gmail.com>
2018-01-16 00:40:10 +0800

    bugs

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+# 邮箱验证url
+Routr::get('/register/verify/{token}',['as' => 'email.verify', 'uses' => 'EmailController@verifyW']);
