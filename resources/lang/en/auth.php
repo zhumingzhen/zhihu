96eefa5d3993baddd17d0a35965662d60e18f167
zhumingzhen <itdream53@gmail.com>
2018-01-21 16:54:25 +0800

    attemplogin

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => '未激活或账号密码错误.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
