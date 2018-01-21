1b663dcbe4d71ed2b272304423085e5db301ac9d
zhumingzhen <itdream53@gmail.com>
2018-01-21 13:31:23 +0800

    login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -52,7 +52,7 @@ class LoginController extends Controller
         }
 
         if ($this->attemptLogin($request)) {
-            flash('欢迎回来!')->success();
+            flash('欢迎回来!', 'success');
             return $this->sendLoginResponse($request);
         }
 
