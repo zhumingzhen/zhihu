1b67bf6aadd59d0f444ab521cdb984e5a439c1a7
zhumingzhen <itdream53@gmail.com>
2018-02-01 22:51:53 +0800

    zhihu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    zhihu 1
+                    zhihu
                 </div>
 
                 <div class="links">
